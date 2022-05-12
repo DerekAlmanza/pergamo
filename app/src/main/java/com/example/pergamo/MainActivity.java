98ae3d3ddf1734bd4c216d4becbc0d393e4cb6c8
VianeyAileen <vianeyborras@ciencias.unam.mx>
2022-05-11 22:54:34 -0500

    Vista Splash y Home terminadas

--- app/src/main/java/com/example/pergamo/MainActivity.java
+++ app/src/main/java/com/example/pergamo/MainActivity.java
@@ -8,24 +8,12 @@ import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
-    Handler handler;
-    
+    //Handler handler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        splash();
-    }
-
-    public void splash(){
-        handler=new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent=new Intent(MainActivity.this,MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        },3000);
+        getSupportActionBar().hide(); // Esconde al label de la aplicación
     }
 }
