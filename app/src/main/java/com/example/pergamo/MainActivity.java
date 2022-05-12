1d25e33ba07fde5180556be633a6e2ed72df86a1
VianeyAileen <vianeyborras@ciencias.unam.mx>
2022-05-11 21:36:36 -0500

    Error en el timepo del splash

--- app/src/main/java/com/example/pergamo/MainActivity.java
+++ app/src/main/java/com/example/pergamo/MainActivity.java
@@ -4,15 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
+    Handler handler;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
-        // setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        splash();
+    }
+
+    public void splash(){
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(MainActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
     }
 }
