cf493552e8f738f663065b03a9eb0145ad04a2a3
VianeyAileen <vianeyborras@ciencias.unam.mx>
2022-05-14 17:24:12 -0500

    Cambiando vista por HomePrincipal

--- app/src/main/java/com/example/pergamo/MainActivity.java
+++ app/src/main/java/com/example/pergamo/MainActivity.java
@@ -1,14 +1,9 @@
 package com.example.pergamo;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
-
-    //Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
