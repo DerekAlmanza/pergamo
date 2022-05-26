43b7cb43d00c7a1633c81618c47fbbfa6cec743b
nachintoch <nachintoch@tutanota.com>
2022-05-26 17:11:19 -0500

    Implementado ejemplo de vista de la cámara embebebida

--- app/src/main/java/com/example/pergamo/EscanearQR.java
+++ app/src/main/java/com/example/pergamo/EscanearQR.java
@@ -1,51 +1,70 @@
 package com.example.pergamo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.zxing.integration.android.IntentIntegrator;
-import com.google.zxing.integration.android.IntentResult;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import com.google.zxing.BarcodeFormat;
+import com.journeyapps.barcodescanner.BarcodeCallback;
+import com.journeyapps.barcodescanner.BarcodeResult;
+import com.journeyapps.barcodescanner.BarcodeView;
+import com.journeyapps.barcodescanner.DefaultDecoderFactory;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class EscanearQR extends AppCompatActivity {
 
-    TextView rutaQR;
+    private BarcodeView barcodeView;
+
+    private BarcodeCallback eventoEscaneo = new BarcodeCallback() {
+        @Override
+        public void barcodeResult(BarcodeResult result) {
+            if (result.getText() == null) return;
+            Toast.makeText(EscanearQR.this, result.getText(), Toast.LENGTH_SHORT).show();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            // TODO Este texto debería ser un recurso de cadena
+            // TODO El texto también podría ser más descriptivo
+            Toast.makeText(this, "Debe otorgar el permiso para usar la cámara",
+                    Toast.LENGTH_SHORT).show();
+            finish();
+        }
         setContentView(R.layout.activity_escanear_qr);
-        getSupportActionBar().hide();
-        rutaQR = findViewById(R.id.rutaQR);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) actionBar.hide();
+        barcodeView = findViewById(R.id.vista_escaner_bv);
         inicializar();
     }
 
-    public void inicializar() {
-        IntentIntegrator integrator = new IntentIntegrator(EscanearQR.this);
-        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
-        integrator.setCameraId(0);
-        integrator.setPrompt("Lector - Finitless");
-        integrator.setBarcodeImageEnabled(true);
-        integrator.initiateScan();
-
+    @Override
+    protected void onResume() {
+        super.onResume();
+        barcodeView.resume();
     }
 
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-
-        if(result != null) {
-            if(result.getContents() == null)
-                Toast.makeText(this, "Lectura cancelada", Toast.LENGTH_LONG).show();
-            else {
-                Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
-                rutaQR.setText(result.getContents());
-            }
-        } else {
-            super.onActivityResult(requestCode, resultCode, data);
-        }
+    @Override
+    protected void onPause() {
+        barcodeView.pause();
+        super.onPause();
     }
+
+    private void inicializar() {
+        List<BarcodeFormat> formatoQr = Arrays.asList(BarcodeFormat.QR_CODE);
+        barcodeView.setDecoderFactory(new DefaultDecoderFactory(formatoQr));
+        barcodeView.decodeContinuous(eventoEscaneo);
+    }
+
+
 }
