d239d4b64733472a8c1b893296613714e8ac35ed
DerekAlmanza <derek.almanza@ciencias.unam.mx>
2022-05-30 21:47:21 -0500

    Implementada vista de Pista

--- app/src/main/java/com/example/pergamo/Pista.java
+++ app/src/main/java/com/example/pergamo/Pista.java
@@ -2,7 +2,9 @@ package com.example.pergamo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Pista extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class Pista extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pista);
+    }
+
+    /**
+     * Linkea el botón Ver puntuación a la pantalla Puntuación
+     * @param view
+     */
+    public void verEscanear(View view) {
+        Intent verEscaner = new Intent(this, EscanearQR.class);
+        startActivity(verEscaner);
+
     }
 }
